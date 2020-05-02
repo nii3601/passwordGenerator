66bc90e4fc1d69eadafd07e34d87b2af87001f4a
nii3601 <32033472+nii3601@users.noreply.github.com>
2020-05-02 10:42:49 +0000

    Initial Commit

---
+++ generate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# THIS SCRIPT IS TO GENERATE X AMOUNTS OF PASSWORDS INTO A CSV FILE
+# DONE BY JONATHAN LAMPTEY JR.
+
+# GLOBAL VARIABLES
+LEN=""
+NUM=""
+
+BASEDIR=$(dirname "$0")
+clear
+
+#make randomGenerator executable
+
+echo -e "Jonathan's Password Generator v1.0\n"
+
+read -p "Length of passwords: " LEN
+read -p "Number of passwords: " NUM
+	
+echo -e "\n ABOUT TO GENERATE $NUM PASSWORDS WITH LENGTH $LEN"
+if [ ! -s "$BASEDIR/passwords.csv" ]; then
+	echo -e "Date,System / SITE,Description,Username,Password" >> $BASEDIR/passwords.csv
+fi
+
+echo -e "WORKING...."
+
+for (( c=1; c<=$NUM; c++  ))
+do
+	TEMP_PASS=`$BASEDIR/randomGenerator $LEN`
+	echo -e ",,,,$TEMP_PASS" >> $BASEDIR/passwords.csv
+done
+echo -e "\nDONE, ALL PASSWORDS SAVED IN passwords.csv"
---
+++ randomGenerator.c
@@ -0,0 +1,33 @@
+// MADE BY JONATHAN LAMPTEY JR.
+// THIS SCRIPT GENERATES A RANDOM PASSWORD BASED ON LENGTH OF PASSWORD WANTED
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+	sleep(1);
+	srand((unsigned int)(time(NULL)));
+	//get length
+	char *input = argv[1];	
+
+	//change length to int
+	int length = atoi(input);
+	
+	//generate random numbers
+	char password[length];
+	for(int i = 0; i < length; i++){
+		int random = 33 + rand() %94;
+		if(random == 94 || random == 61 || random == 44){
+			random = 76;
+		}
+		password[i] = random;
+	}
+	password[length] = '\0'; //adding sentinal character
+	printf("%s\n", password); //printing password
+	return 0;
+}
+
